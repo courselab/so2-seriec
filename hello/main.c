c8aff35beae9da8dfe0cf1f5aa21e0ebc10bd62c
Arthur Grillo <arthurgrillo@riseup.net>
2025-06-14 17:32:12 -0300

    hello: Add exercise template

---
+++ hello/main.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 Arthur Grillo <arthurgrillo@riseup.net>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  Arthur Grillo <arthurgrillo@riseup.net>
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf ("Hello World");
+	return 0;
+}
---
+++ hello/stdio.h
@@ -0,0 +1,16 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 Arthur Grillo <arthurgrillo@riseup.net>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  Arthur Grillo <arthurgrillo@riseup.net>
+ */
+
+#ifndef E8_H
+#define E8_H
+
+void __attribute__((fastcall, naked)) printf(const char *); 
+
+#endif	
